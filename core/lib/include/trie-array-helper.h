797edc20650d2fb0082e2dab49625ebfa596490d
feredev <Hector1567XD@gmail.com>
2022-02-04 20:58:02 -0500

    Added TrieArrayHelper

---
+++ core/lib/include/trie-array-helper.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "../../tad/include/trie-tree.h"
+
+int trieArrayGetLength(struct TrieNode ** trieArray);
+
+void trieArrayAddNodo(struct TrieNode ** trieArray, struct TrieNode *trieNode);
---
+++ core/lib/trie-array-helper.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "../../tad/include/trie-tree.h"
+
+int trieArrayGetLength(struct TrieNode ** trieArray)
+{
+    int arrayLen = 0;
+    if (trieArray)
+      while (trieArray[arrayLen] != NULL){
+        arrayLen++;
+      };
+    return arrayLen;
+}
+
+void trieArrayAddNodo(struct TrieNode ** trieArray, struct TrieNode *trieNode)
+{
+    int currentSize = trieArrayGetLength(trieArray);
+    if (currentSize > 0) {
+      trieArray = realloc(trieArray, sizeof(trieArray) * (currentSize+2));
+    }else{
+      trieArray = malloc(sizeof(trieArray) * 2);
+    }
+    trieNode[currentSize] = trieNode;
+    trieNode[currentSize+1] = NULL;
+}
