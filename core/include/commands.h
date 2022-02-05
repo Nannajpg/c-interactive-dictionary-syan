f32566baedda61d3d440174780625639f3f789fb
feredev <Hector1567XD@gmail.com>
2022-02-04 22:52:35 -0500

    Sponje bobo poening 3

--- core/include/commands.h
+++ core/include/commands.h
@@ -6,6 +6,9 @@
 #define SINONIMOS_COMMAND "s"
 #define ANTONIMOS_COMMAND "a"
 #define EXPRESSION_COMMAND "e"
+#define SINONIMOS_LARGE_COMMAND "sinonimos"
+#define ANTONIMOS_LARGE_COMMAND "antonimos"
+#define EXPRESSION_LARGE_COMMAND "expresion"
 #define AYUDA_COMMAND "ayuda"
 #define QUIT_COMMAND "salir"
 
