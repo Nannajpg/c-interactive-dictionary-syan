46df32c363850dfb7c8d66fc392a1669073fcdba
yisustxz <jesusoortiz2410@gmail.com>
2022-02-04 21:10:21 -0500

    version 2.0

--- core/lib/regular-expression-for-trie.c
+++ core/lib/regular-expression-for-trie.c
@@ -4,9 +4,25 @@
 #include <ctype.h>
 
 
-void expresionRegularToTrie(struct Trienode *root, const char word){
+// function to display the content of Trie
+void display(struct TrieNode* root, TrieNode ** arregloNodos, char* expression, char str[], int level)
+{
+    if (trieGetisWord(root))
+    {
+        str[level] = '\0';
+        if (funcionRaraMatchExpression(expression, str) {
+                agregarNodoAArregloDeNodos(arregloNodos, root);
+       }
+       // printf(str);
+    }
 
-  
-
-
+    int i;
+    for (i = 0; i < ALPHABET_SIZE; i++)
+    {
+        if (root->children[i])
+        {
+            str[level] = i + 'a';
+            display(root->children[i], str, level + 1);
+        }
+    }
 }
